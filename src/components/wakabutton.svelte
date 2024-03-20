9326db096e71d71d0fba446f24b807f5b43f52bb
Shlok-Bhakta <62256489+Shlok-Bhakta@users.noreply.github.com>
2024-03-20 02:27:07 +0000

    init

---
+++ src/components/wakabutton.svelte
@@ -0,0 +1,15 @@
+<script>
+  let x = 0;
+  let y = 0;
+  console.log("locked and loaded");
+  function yPlusAValue(value) {
+    return value + y;
+  }
+
+  $: total = yPlusAValue(x);
+</script>
+
+Total: {total}
+<button on:click={() => x++}> Increment X </button>
+
+<button on:click={() => y++}> Increment Y </button>
