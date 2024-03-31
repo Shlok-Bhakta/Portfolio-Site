4244ccb1302426b7ff901a0f88220e804e810d0c
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-03-31 23:05:10 +0000

    🍔 start hamburger menu work

---
+++ src/components/hamburger.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+/>
+<div class="group/burger">
+  <button class="flex sm:flex md:hidden py-1 px-3 mx-3 mr-4"
+    ><span class="material-symbols-outlined text-9xl text-text"> menu </span>
+  </button>
+</div>
