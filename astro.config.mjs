dba5b6c44b1e80cfd45ddfd4409aaecbf84c70d2
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-07-21 03:53:35 +0000

    Fixed Errors and Got Initial Build 🪄

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [sitemap(), svelte(), tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
