d39f4e96fcb9a336ae89cce06881b848c4aaf639
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-07-22 22:24:33 +0000

    Giving up on Forgejo and just going github 🥲

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,9 @@ import sitemap from "@astrojs/sitemap";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   integrations: [sitemap(), svelte(), tailwind()],
-  output: "server",
-  adapter: node({
-    mode: "standalone"
-  })
 });
