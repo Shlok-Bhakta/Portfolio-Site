cad64d4a56b48f1067a17df2013e9f1609275006
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-07-29 05:38:31 +0000

    Removed The Janky Background Stars ❌

--- src/components/aceternity/stars/Starsbg.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { cn } from "./cn.ts";
-    import Illustration from "./Illustration.svelte";
-</script>
-
-<div class="h-full flex items-center justify-center">
-    <Illustration stars={document.documentElement.offsetHeight / 1} />
-</div>
-<div class="px-2 pb-6"><slot /></div>
