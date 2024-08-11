dcc8dfb3bc9a79ba6f7a13638f53f9496eddffae
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-08-11 06:18:34 +0000

    Removed old Bento Grid

--- src/components/acebento/bentogrid.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import { cn } from "./cn.ts";
-
-    export let className: string | undefined = undefined;
-</script>
-
-<div class={cn("mx-auto grid max-w-7xl grid-cols-1 gap-4 md:auto-rows-[18rem] md:grid-cols-3 ", className)}>
-    <slot />
-</div>
