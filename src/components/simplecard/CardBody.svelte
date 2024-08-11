1833bed684e61e5dc4af47e7a10b6993bba24195
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-08-11 06:18:54 +0000

    Created First Description Card

---
+++ src/components/simplecard/CardBody.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { cn } from "./utils.ts";
+
+    let className: any = "";
+    export { className as class };
+    let cardContent = {
+        title: "Svelte is Vibe",
+        description: "Svelte is a fun way to build web applications.",
+    };
+</script>
+
+<div class={cn("text-left", className)}>
+    <h3 class="text-lg font-bold mb-1 text-gray-100">
+        {cardContent.title}
+    </h3>
+    <p class="text-gray-300">
+        {cardContent.description}
+    </p>
+</div>
---
+++ src/components/simplecard/CardContainer.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Ellipses from "./Ellipses.svelte";
+</script>
+
+<div class="relative w-full rounded-lg border border-dashed px-4 border-zinc-800 sm:px-6 md:px-8 bg-[#09090B] shadow-sm">
+    <div class="absolute left-0 top-4 -z-0 h-px w-full bg-zinc-700 sm:top-6 md:top-8"></div>
+    <div class="absolute bottom-4 left-0 z-0 h-px w-full bg-zinc-700 sm:bottom-6 md:bottom-8"></div>
+    <div class="relative w-full border-x border-zinc-700">
+        <Ellipses />
+        <div class="relative z-20 w-full py-8">
+            <slot>Default</slot>
+        </div>
+    </div>
+</div>
