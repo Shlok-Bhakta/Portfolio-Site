ada751986d40bf6cfb7e6f7399a346b18d106af7
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-08-11 06:22:10 +0000

    Changed Colors to be more on brand

--- src/components/simplecard/CardContainer.svelte
+++ src/components/simplecard/CardContainer.svelte
@@ -2,10 +2,10 @@
     import Ellipses from "./Ellipses.svelte";
 </script>
 
-<div class="relative w-full rounded-lg border border-dashed px-4 border-zinc-800 sm:px-6 md:px-8 bg-[#09090B] shadow-sm">
-    <div class="absolute left-0 top-4 -z-0 h-px w-full bg-zinc-700 sm:top-6 md:top-8"></div>
-    <div class="absolute bottom-4 left-0 z-0 h-px w-full bg-zinc-700 sm:bottom-6 md:bottom-8"></div>
-    <div class="relative w-full border-x border-zinc-700">
+<div class="relative w-full rounded-lg border border-dashed px-4 border-surface0 sm:px-6 md:px-8 bg-crust shadow-sm">
+    <div class="absolute left-0 top-4 -z-0 h-px w-full bg-surface0 sm:top-6 md:top-8"></div>
+    <div class="absolute bottom-4 left-0 z-0 h-px w-full bg-surface0 sm:bottom-6 md:bottom-8"></div>
+    <div class="relative w-full border-x border-surface0">
         <Ellipses />
         <div class="relative z-20 w-full py-8">
             <slot>Default</slot>
