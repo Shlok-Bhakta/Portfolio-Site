9cebff3972813a34896a5a7fc5d53dc605d651ba
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-08-25 18:33:30 -0500

    Added Meteor Effect

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,18 +5,27 @@ export default {
 		extend: {
 			animation: {
 			  "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
+			  "meteor": "meteor 5s linear infinite",
 			},
 			keyframes: {
 			  "border-beam": {
-				"100%": {
-				  "offset-distance": "100%",
-				},
+					"100%": {
+						"offset-distance": "100%",
+					},
 			  },
-			},
+			  "meteor": {
+					"0%": { transform: "rotate(215deg) translateX(0)", opacity: 1 },
+					"70%": { opacity: 1 },
+					"100%": {
+						transform: "rotate(215deg) translateX(-500px)",
+						opacity: 0,
+					},
+				},
 		  },
+		},
 		fontFamily: {
 			'display': ['CaskaydiaCoveNF', "/fonts/CaskaydiaCoveNF.ttf"],
-		  }
-	},
+		}
+		},
 	plugins: [require("@catppuccin/tailwindcss")],
-}
+	}
