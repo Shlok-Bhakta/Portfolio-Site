88e2bdca9639d164bf7bfff6eaae0f5de28b03ad
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-08-25 22:53:34 -0500

    randomize z index for overlap 📔

--- src/components/meteors/Meteors.svelte
+++ src/components/meteors/Meteors.svelte
@@ -15,6 +15,7 @@
         animationDuration: Math.floor(Math.random() * 8 + 2.9) + "s",
         fromCol: trailColor[Math.floor(Math.random() * 7)],
         viaCol: trailColorMid[Math.floor(Math.random() * 7)],
+        zLevel: Math.floor(Math.random() * 20)
       }));
       meteorStyles = styles;
     };
@@ -29,7 +30,7 @@
     class={cn(
     "pointer-events-none absolute left-1/2 top-1/2 size-[2.4px] rotate-[215deg] animate-meteor rounded-full bg-crust shadow-crust"
     )}
-    style="top: {style.top}px; left: {style.left}; animation-delay: {style.animationDelay}; animation-duration: {style.animationDuration};"
+    style="top: {style.top}px; left: {style.left}; animation-delay: {style.animationDelay}; animation-duration: {style.animationDuration}; z-index: {style.zLevel};"
 >
     <!-- Meteor Tail  -->
     <div
