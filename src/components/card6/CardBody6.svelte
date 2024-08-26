4c32675afb3ebe8c0d45cf0af6f9b374fe8dd4ba
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-08-25 23:12:50 -0500

    added a 3d card 💳

---
+++ src/components/card6/CardBody6.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { cn } from "./utils.ts";
+
+  let card = {
+    title: "3D Card",
+    desc: "Don't take above two words seriously, I just made them up.",
+  };
+  let className = "p-8 h-full w-full";
+  export { className as class };
+</script>
+
+<div class={cn("text-left", className)}>
+  <h3 class="text-lg mb-1 font-bold text-text h-full w-full">
+    {card.title}
+  </h3>
+  <p class="text-text h-full w-full">
+    {card.desc}
+  </p>
+</div>
---
+++ src/components/card6/SimpleCardVariant6.svelte
@@ -0,0 +1,11 @@
+<script>
+  import CardBody6 from "./CardBody6.svelte";
+</script>
+
+<div>
+  <div
+    class="border border-base relative shadow-[0px_4px_0px_0px_rgba(108,112,134,0.5)] rounded-xl bg-mantle h-full w-full"
+  >
+    <CardBody6 class="p-8" />
+  </div>
+</div>
