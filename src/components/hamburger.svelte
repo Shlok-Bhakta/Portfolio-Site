1c4226154b7da648c30ebfc3d688b6995783a912
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-09-14 01:53:19 -0500

    remove google icons for mobile users

--- src/components/hamburger.svelte
+++ src/components/hamburger.svelte
@@ -6,10 +6,9 @@
   }
 </script>
 
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <div class="">
   <button on:click={toggle} class="flex md:hidden mx-3 mr-4">
-    <span class="material-symbols-outlined text-9xl text-text py-1.5 px-3 bg-inherit transition-all ease-in-out delay-50 hover:bg-surface1 rounded-md"> menu </span>
+    <img src="/menu.svg" alt="Menu Icon" class="text-text py-1.5 px-3 bg-inherit transition-all ease-in-out delay-50 hover:bg-surface1 rounded-md w-12">
   </button>
 
   {#if clicked}
@@ -20,7 +19,8 @@
     >
       <div class="flex flex-col space-y-3 items-center">
         <button on:click={toggle} class="flex mx-3 mr-4">
-          <span class="material-symbols-outlined text-9xl text-text py-1.5 px-1.5 transition-all ease-in-out delay-50 hover:bg-surface1 rounded-md"> close </span>
+          <img src="/close.svg" alt="Close Menu" class="text-text py0.5 px-1.5 transition-all ease-in-out delay-50 bg-surface0 hover:bg-surface1 rounded-md w-12">
+        
         </button>
         <a href="/" class="bg-surface2 rounded-md py-7 w-[80svw] mx-3 text-text nerdfont text-center"> Home </a>
         <a href="/blog/" class="bg-surface2 rounded-md py-7 w-[80svw] mx-3 text-text nerdfont text-center"> Blog </a>
